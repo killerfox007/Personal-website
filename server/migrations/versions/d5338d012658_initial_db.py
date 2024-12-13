e8d02e0544ffc28aafdcebc29d44cc0f46b2704d
Kaleb Fox <kalebfox7@yahoo.com>
2024-12-13 17:17:14 -0500

    initial commit

---
+++ server/migrations/versions/d5338d012658_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: d5338d012658
+Revises: 
+Create Date: 2024-12-13 17:16:57.479043
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd5338d012658'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
