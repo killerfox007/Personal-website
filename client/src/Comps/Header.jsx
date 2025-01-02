d6502d3e1b73d1556a4667c10dac4274df637407
Kaleb Fox <kalebfox7@yahoo.com>
2025-01-01 23:31:05 -0500

    finshed some css taking a break

---
+++ client/src/Comps/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header classname="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zink-900 to-zink-900/0">
+        <div className="max-w-screen-2xl w-full mx-auto px-4">
+            <h1>
+                <a href= ></a>
+            </h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
